8c2fad3d4137d1b93bcaf5d89907acc810264ddb
Laura Siu <lmsiu@cpp.edu>
2021-09-23 08:25:53 -0700

    added the function preview file header.h

---
+++ header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER
+
+//Please change/update as needed
+#define HEADER
+double getCarRentalFees();
+double getConfrenceFees();
+double getParkingFees();
+double getMilesDriven();
+double getHotelExpenses();
+
+double getBreakfastAmount();
+double getLunchAmount();
+double getDinnerAmount();
+double getRoundTripCost();
+
+#endif
